609fbc06ed5baf8778d03e933362805a614af7fb
yoana <yewonyoana@gmail.com>
2023-09-30 03:28:22 +0900

    Fixed Bug

--- src/middlewares.js
+++ src/middlewares.js
@@ -21,7 +21,6 @@ const s3VideoUploader = multerS3({
 	s3: s3,
 	bucket: "wetube-reloaded-2023/videos",
 	acl: "public-read",
-	contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 export const localsMiddleware = (req, res, next) => {
