f245e7134b9235f443d72516795e1775e0977cbe
yoana <yewonyoana@gmail.com>
2023-09-30 22:29:50 +0900

    Fixed Bugs

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,52 +1,273 @@
+// import User from "../models/User";
+// import Video from "../models/Video";
+// import fetch from "node-fetch";
+// import bcrypt from "bcrypt";
+
+// export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+// export const postJoin = async (req, res) => {
+// 	const { name, username, email, password, password2, location } = req.body;
+// 	const pageTitle = "Join";
+// 	if (password !== password2) {
+// 		return res.status(400).render("join", {
+// 			pageTitle,
+// 			errorMessage: "Password does not match.",
+// 		});
+// 	}
+// 	const exists = await User.exists({ $or: [{ username }, { email }] });
+// 	if (exists) {
+// 		return res.status(400).render("join", {
+// 			pageTitle,
+// 			errorMessage: "This username/e-mail is already taken.",
+// 		});
+// 	}
+// 	try {
+// 		await User.create({ name, username, email, password, location });
+// 		return res.redirect("/login");
+// 	} catch (error) {
+// 		return res.status(400).render("join", {
+// 			pageTitle: "Join",
+// 			errorMessage: error._message,
+// 		});
+// 	}
+// };
+
+// export const getLogin = (req, res) =>
+// 	res.render("login", { pageTitle: "Login" });
+
+// export const postLogin = async (req, res) => {
+// 	const { username, password } = req.body;
+// 	const pageTitle = "Login";
+// 	//check if account exists
+// 	const user = await User.findOne({ username, socialOnly: false });
+// 	if (!user) {
+// 		return res.status(400).render("login", {
+// 			pageTitle,
+// 			errorMessage: "An account with this username does not exist.",
+// 		});
+// 	}
+// 	//check if password exists
+// 	const ok = await bcrypt.compare(password, user.password);
+// 	if (!ok) {
+// 		return res.status(400).render("login", {
+// 			pageTitle,
+// 			errorMessage: "Wrong password",
+// 		});
+// 	}
+// 	req.session.loggedIn = true;
+// 	//different for every browser
+// 	req.session.user = user;
+// 	return res.redirect("/");
+// };
+
+// export const startGithubLogin = (req, res) => {
+// 	const baseUrl = "https://github.com/login/oauth/authorize";
+// 	const config = {
+// 		client_id: process.env.GH_CLIENT,
+// 		allow_signup: false,
+// 		scope: "read:user user:email",
+// 	};
+// 	const params = new URLSearchParams(config).toString();
+// 	const finalUrl = `${baseUrl}?${params}`;
+// 	return res.redirect(finalUrl);
+// };
+
+// export const finishGithubLogin = async (req, res) => {
+// 	const baseUrl = "https://github.com/login/oauth/access_token";
+// 	const config = {
+// 		client_id: process.env.GH_CLIENT,
+// 		client_secret: process.env.GH_SECRET,
+// 		code: req.query.code,
+// 	};
+// 	const params = new URLSearchParams(config).toString();
+// 	const finalUrl = `${baseUrl}?${params}`;
+// 	const tokenRequest = await (
+// 		await fetch(finalUrl, {
+// 			method: "POST",
+// 			headers: {
+// 				Accept: "application/json",
+// 			},
+// 		})
+// 	).json();
+// 	if ("access_token" in tokenRequest) {
+// 		//access api
+// 		const { access_token } = tokenRequest;
+// 		const apiUrl = "https://api.github.com";
+// 		const userData = await (
+// 			await fetch(`${apiUrl}/user`, {
+// 				headers: { Authorization: `token ${access_token}` },
+// 			})
+// 		).json();
+// 		console.log(userData);
+// 		const emailData = await (
+// 			await fetch(`${apiUrl}/user/emails`, {
+// 				headers: { Authorization: `token ${access_token}` },
+// 			})
+// 		).json();
+// 		//find things in array
+// 		const emailObj = emailData.find(
+// 			(email) => email.primary === true && email.verified === true
+// 		);
+// 		if (!emailObj) {
+// 			return res.redirect("/login");
+// 		}
+// 		let user = await User.findOne({ email: emailObj.email });
+// 		if (!user) {
+// 			//create account
+// 			user = await User.create({
+// 				avatarUrl: userData.avatar_url,
+// 				name: userData.name ? userData.name : "Unknown",
+// 				username: userData.login,
+// 				email: emailObj.email,
+// 				password: "",
+// 				socialOnly: true,
+// 				location: userData.location,
+// 			});
+// 		}
+// 		req.session.loggedIn = true;
+// 		req.session.user = user;
+// 		return res.redirect("/");
+// 	} else {
+// 		return res.redirect("/login");
+// 	}
+// };
+
+// export const logout = (req, res) => {
+// 	req.session.user = null;
+// 	res.locals.loggedInUser = req.session.user;
+// 	req.session.loggedIn = false;
+// 	req.flash("info", "Bye!");
+// 	return res.redirect("/");
+// };
+
+// export const getEdit = (req, res) => {
+// 	return res.render("edit-profile", { pageTitle: "Edit Profile" });
+// };
+
+// export const postEdit = async (req, res) => {
+// 	const {
+// 		session: {
+// 			user: { _id, avatarUrl },
+// 		},
+// 		body: { name, email, username, location },
+// 		file,
+// 	} = req;
+
+// 	const isCloudtype = process.env.NODE_ENV === "production";
+
+// 	const updatedUser = await User.findByIdAndUpdate(
+// 		_id,
+// 		{
+// 			avatarUrl: file ? (isCloudtype ? file.location : file.path) : avatarUrl,
+// 			name,
+// 			email,
+// 			username,
+// 			location,
+// 		},
+// 		{ new: true }
+// 	);
+// 	req.session.user = updatedUser;
+// 	return res.redirect("/user/edit");
+// };
+
+// export const getChangePassword = (req, res) => {
+// 	if (req.session.user.socialOnly === true) {
+// 		req.flash("error", "Cannot Change Password");
+// 		return res.redirect("/");
+// 	}
+// 	return res.render("users/change-password", { pageTitle: "Change Password" });
+// };
+// export const postChangePassword = async (req, res) => {
+// 	//send notification that password has been changed
+// 	const {
+// 		session: {
+// 			user: { _id },
+// 		},
+// 		body: { oldPassword, newPassword, newPasswordConfirmation },
+// 	} = req;
+// 	const user = await User.findById(_id);
+// 	const ok = await bcrypt.compare(oldPassword, user.password);
+// 	if (!ok) {
+// 		return res.status(400).render("users/change-password", {
+// 			pageTitle: "Change Password",
+// 			errorMessage: "Incorrect current password",
+// 		});
+// 	}
+// 	if (newPassword !== newPasswordConfirmation) {
+// 		return res.status(400).render("users/change-password", {
+// 			pageTitle: "Change Password",
+// 			errorMessage: "Confirmation password does not match.",
+// 		});
+// 	}
+// 	user.password = newPassword;
+// 	await user.save();
+// 	req.flash("info", "Password Updated");
+// 	return res.redirect("/user/logout");
+// };
+
+// export const see = async (req, res) => {
+// 	const { id } = req.params;
+// 	const user = await User.findById(id).populate("videos");
+
+// 	if (!user) {
+// 		return res.status(404).render("404", { pageTitle: "User not found." });
+// 	}
+// 	return res.render("users/profile", {
+// 		pageTitle: user.name,
+// 		user,
+// 	});
+// };
+
 import User from "../models/User";
-import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-
 export const postJoin = async (req, res) => {
 	const { name, username, email, password, password2, location } = req.body;
 	const pageTitle = "Join";
 	if (password !== password2) {
 		return res.status(400).render("join", {
 			pageTitle,
-			errorMessage: "Password does not match.",
+			errorMessage: "Password confirmation does not match.",
 		});
 	}
 	const exists = await User.exists({ $or: [{ username }, { email }] });
 	if (exists) {
 		return res.status(400).render("join", {
 			pageTitle,
-			errorMessage: "This username/e-mail is already taken.",
+			errorMessage: "This username/email is already taken.",
 		});
 	}
 	try {
-		await User.create({ name, username, email, password, location });
+		await User.create({
+			name,
+			username,
+			email,
+			password,
+			location,
+		});
 		return res.redirect("/login");
 	} catch (error) {
 		return res.status(400).render("join", {
-			pageTitle: "Join",
+			pageTitle: "Upload Video",
 			errorMessage: error._message,
 		});
 	}
 };
-
 export const getLogin = (req, res) =>
 	res.render("login", { pageTitle: "Login" });
 
 export const postLogin = async (req, res) => {
 	const { username, password } = req.body;
 	const pageTitle = "Login";
-	//check if account exists
 	const user = await User.findOne({ username, socialOnly: false });
 	if (!user) {
 		return res.status(400).render("login", {
 			pageTitle,
-			errorMessage: "An account with this username does not exist.",
+			errorMessage: "An account with this username does not exists.",
 		});
 	}
-	//check if password exists
 	const ok = await bcrypt.compare(password, user.password);
 	if (!ok) {
 		return res.status(400).render("login", {
@@ -55,7 +276,6 @@ export const postLogin = async (req, res) => {
 		});
 	}
 	req.session.loggedIn = true;
-	//different for every browser
 	req.session.user = user;
 	return res.redirect("/");
 };
@@ -90,33 +310,34 @@ export const finishGithubLogin = async (req, res) => {
 		})
 	).json();
 	if ("access_token" in tokenRequest) {
-		//access api
 		const { access_token } = tokenRequest;
 		const apiUrl = "https://api.github.com";
 		const userData = await (
 			await fetch(`${apiUrl}/user`, {
-				headers: { Authorization: `token ${access_token}` },
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
 			})
 		).json();
-		console.log(userData);
 		const emailData = await (
 			await fetch(`${apiUrl}/user/emails`, {
-				headers: { Authorization: `token ${access_token}` },
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
 			})
 		).json();
-		//find things in array
 		const emailObj = emailData.find(
 			(email) => email.primary === true && email.verified === true
 		);
 		if (!emailObj) {
+			// set notification
 			return res.redirect("/login");
 		}
 		let user = await User.findOne({ email: emailObj.email });
 		if (!user) {
-			//create account
 			user = await User.create({
 				avatarUrl: userData.avatar_url,
-				name: userData.name ? userData.name : "Unknown",
+				name: userData.name,
 				username: userData.login,
 				email: emailObj.email,
 				password: "",
@@ -133,17 +354,13 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-	req.session.user = null;
-	res.locals.loggedInUser = req.session.user;
-	req.session.loggedIn = false;
-	req.flash("info", "Bye!");
+	req.session.destroy();
+	req.flash("info", "Bye Bye");
 	return res.redirect("/");
 };
-
 export const getEdit = (req, res) => {
 	return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
-
 export const postEdit = async (req, res) => {
 	const {
 		session: {
@@ -152,13 +369,10 @@ export const postEdit = async (req, res) => {
 		body: { name, email, username, location },
 		file,
 	} = req;
-
-	const isCloudtype = process.env.NODE_ENV === "production";
-
 	const updatedUser = await User.findByIdAndUpdate(
 		_id,
 		{
-			avatarUrl: file ? (isCloudtype ? file.location : file.path) : avatarUrl,
+			avatarUrl: file ? file.path : avatarUrl,
 			name,
 			email,
 			username,
@@ -167,18 +381,17 @@ export const postEdit = async (req, res) => {
 		{ new: true }
 	);
 	req.session.user = updatedUser;
-	return res.redirect("/user/edit");
+	return res.redirect("/users/edit");
 };
 
 export const getChangePassword = (req, res) => {
 	if (req.session.user.socialOnly === true) {
-		req.flash("error", "Cannot Change Password");
+		req.flash("error", "Can't change password.");
 		return res.redirect("/");
 	}
 	return res.render("users/change-password", { pageTitle: "Change Password" });
 };
 export const postChangePassword = async (req, res) => {
-	//send notification that password has been changed
 	const {
 		session: {
 			user: { _id },
@@ -190,25 +403,30 @@ export const postChangePassword = async (req, res) => {
 	if (!ok) {
 		return res.status(400).render("users/change-password", {
 			pageTitle: "Change Password",
-			errorMessage: "Incorrect current password",
+			errorMessage: "The current password is incorrect",
 		});
 	}
 	if (newPassword !== newPasswordConfirmation) {
 		return res.status(400).render("users/change-password", {
 			pageTitle: "Change Password",
-			errorMessage: "Confirmation password does not match.",
+			errorMessage: "The password does not match the confirmation",
 		});
 	}
 	user.password = newPassword;
 	await user.save();
-	req.flash("info", "Password Updated");
-	return res.redirect("/user/logout");
+	req.flash("info", "Password updated");
+	return res.redirect("/users/logout");
 };
 
 export const see = async (req, res) => {
 	const { id } = req.params;
-	const user = await User.findById(id).populate("videos");
-
+	const user = await User.findById(id).populate({
+		path: "videos",
+		populate: {
+			path: "owner",
+			model: "User",
+		},
+	});
 	if (!user) {
 		return res.status(404).render("404", { pageTitle: "User not found." });
 	}
