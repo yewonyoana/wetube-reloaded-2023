d27d1cbc50a089e7776a193acfd10a655e6e1bbf
yoana <yewonyoana@gmail.com>
2023-10-01 01:21:10 +0900

    Fixed Bugs

--- src/server.js
+++ src/server.js
@@ -35,19 +35,24 @@ app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
-});
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+	})
+);
 // app.use((req, res, next) => {
-// 	res.header("Cross-Origin-Embedder-Policy", "require-corp");
-// 	res.header("Cross-Origin-Opener-Policy", "same-origin");
+// 	res.setHeader("Access-Control-Allow-Origin", "*");
+// 	res.header(
+// 		"Access-Control-Allow-Headers",
+// 		"Origin, X-Requested-With, Content-Type, Accept"
+// 	);
 // 	next();
 // });
+app.use((req, res, next) => {
+	res.header("Cross-Origin-Embedder-Policy", "require-corp");
+	res.header("Cross-Origin-Opener-Policy", "same-origin");
+	next();
+});
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/user", userRouter);
